978c6b0134748ea7872bba40a2ea8ea39a51baf2
dawish <742637254@qq.com>
2017-05-24 23:50:16 +0800

    # add application

--- app/src/main/java/app/MDApplication.kt
+++ app/src/main/java/app/MDApplication.kt
@@ -6,12 +6,16 @@ import android.app.Application
  * Created by dawish on 2017/5/22.
  */
 class MDApplication : Application() {
-
+    init {
+        instance = this
+    }
     override fun onCreate() {
         super.onCreate()
 
-
-
     }
 
+    companion object {
+        lateinit var instance: MDApplication
+
+    }
 }
